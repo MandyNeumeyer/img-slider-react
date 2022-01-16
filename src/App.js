9ac39ae1117d7782975112d88de8ab2a208a387c
MandyNeumeyer <Mandy.Neumeyer@web.de>
2022-01-17 00:16:37 +0100

    style: all img incl.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Slider from './components/Slider'
+
 import img1 from './images/img1.jpg';
 import img2 from './images/img2.jpg';
 import img3 from './images/img3.jpg';
-import img4 from './images/img4.jpg';
-import img5 from './images/img5.jpg';
+
 
 
 import './App.css'
 
 function App() {
 
-  const images=[img1,img2,img3, img4, img5]
+  const images=[img1,img2, img3]
 
   return (
     <div className="App">
